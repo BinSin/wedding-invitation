5b3ca8bc1f4a3276eedb5b9149c2ee5be06bbb85
BinSin <binyong318@gmail.com>
2024-12-09 00:02:30 +0900

    feat: init

---
+++ src/test/java/com/ym/weddinginvitation/WeddingInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.weddinginvitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingInvitationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
